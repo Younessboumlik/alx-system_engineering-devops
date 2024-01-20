bb8928a370ee8d1f3c4d609e58315aa3228346f6
Youness Boumlik <younessboumlik@gmail.com>
2024-01-20 03:54:50 +0100

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#this is my first puppet program
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
