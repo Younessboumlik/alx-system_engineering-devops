870927d2d28cd969f73c31ff46f8c3d8f4ce8d71
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-02-15 16:45:28 +0100

    task1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/",strict_slashes=False)
+def main():
+  return "Hello HBNB!"
+@app.route("/hbnb",strict_slashes=False)
+def main2():
+  return "HBNB"
+
+app.run(port = 5000,host="0.0.0.0")
